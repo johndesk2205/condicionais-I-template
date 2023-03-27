5a3751b0edeec9d37db232d81bc6f8e80d8a4441
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-27 11:43:50 -0300

    Resolução dos exercicios

---
+++ script.js
@@ -0,0 +1,60 @@
+//Pratica Guiada
+
+const booleano1 = true
+const booleano2 = false
+
+// if(booleano1===true){
+//     alert('booleano')
+// }else{
+//     alert('booleano1 não é igual a true')
+// }
+
+const booleano3 = true
+
+// if (booleano1 === booleano2){
+//     alert('booleano1 é igual a booleano2')
+// } else if(booleano2 === booleano3){
+//     alert('booleano2 é igual a booleano3')
+// } else if(booleano1 === booleano3){
+//     alert('booleano1 é igual a booleano3')
+// } else{
+//     alert('Não exite valores iguais')
+// }
+
+const idade = 25
+
+// if (idade >= 18) {
+//     if (idade >= 70) {
+//         alert("Pode votar, mas é facultativo")
+//     } else {
+//         alert("Pode votar, é obrigatório")
+//     }
+// } else{
+//     if(idade>=16){ 
+//         alert("Pode votar, mas é facultativo")
+//     } else{
+//         alert("Não pode votar")
+//     }
+// }
+
+
+//Exercicio de Fixação
+
+const media = 10
+
+if (media >= 5 && media <=10) {
+    alert("Você foi aprovado")
+} else if (media >= 3 && media < 5) {
+    alert("Recuperação")
+
+} else if (media < 3) {
+    alert("Você foi reprovado")
+
+} else {
+    alert("Dado inválido")
+}
+
+
+
+
+
